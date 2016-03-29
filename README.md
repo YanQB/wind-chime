fbd6d1b795e8a5fdeddf947a4ad8dd0fa24a8c0c
yanqingbin <yanqb@truekey.mobi>
2016-03-29 14:48:56 +0800

    20160329 add mViewHolder

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This is one for the collection and study of Dome APP.
 first alter create。
 本地库进行修改。
+03-29 add mViewHolder。
--- RankRaise/app/src/main/java/chime/wind/rankraise/MainActivity.java
+++ RankRaise/app/src/main/java/chime/wind/rankraise/MainActivity.java
@@ -6,11 +6,18 @@ import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.lang.reflect.Method;
+
+import chime.wind.rankraise.uitls.mViewHolder;
 
 public class MainActivity extends Activity {
     private EditText mEditText;
@@ -117,6 +124,34 @@ public class MainActivity extends Activity {
         int visibility = keyboardView.getVisibility();
         if (visibility == View.VISIBLE) {
             keyboardView.setVisibility(View.INVISIBLE);
+        }
+    }
+    class apapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if(null == view){
+                view = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_item_view,viewGroup);
+            }
+            TextView mTv_item_content = mViewHolder.get(view,R.id.mTv_item_content);
+            Button mBu_item_certain = mViewHolder.get(view,R.id.mBu_item_certain);
+
+            return view;
         }
     }
 }
