00a590fc5cf1024a35c4e2bca6584520fdfcb9a3
yanqingbin <yanqb@truekey.mobi>
2016-03-29 10:47:32 +0800

    20160329 add RankRaise

---
+++ RankRaise/app/src/main/java/chime/wind/rankraise/MainActivity.java
@@ -0,0 +1,122 @@
+package chime.wind.rankraise;
+
+import android.app.Activity;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+import java.lang.reflect.Method;
+
+public class MainActivity extends Activity {
+    private EditText mEditText;
+    KeyboardView keyboardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.editText);
+        //mEditText.setInputType(InputType.TYPE_NULL);
+        //mEditText.setShowSoftInputOnFocus(false);
+        if (android.os.Build.VERSION.SDK_INT <= 10) {//4.0以下 danielinbiti
+            mEditText.setInputType(InputType.TYPE_NULL);
+        } else {
+            this.getWindow().setSoftInputMode(
+                    WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+            try {
+                Class<EditText> cls = EditText.class;
+                Method setShowSoftInputOnFocus;
+                setShowSoftInputOnFocus = cls.getMethod("setShowSoftInputOnFocus",
+                        boolean.class);
+                setShowSoftInputOnFocus.setAccessible(true);
+                setShowSoftInputOnFocus.invoke(mEditText, false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        mEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showKeyboard();
+            }
+        });
+        keyboardView = (KeyboardView)findViewById(R.id.keyboard_view);
+        keyboardView.setKeyboard(new Keyboard(this, R.xml.mykey));
+        keyboardView.setEnabled(true);
+        keyboardView.setPreviewEnabled(true);
+        keyboardView.setPreviewEnabled(false);
+        keyboardView.setOnKeyboardActionListener(new KeyboardView.OnKeyboardActionListener() {
+            @Override
+            public void onPress(int i) {
+
+            }
+
+            @Override
+            public void onRelease(int i) {
+
+            }
+
+            @Override
+            public void onKey(int i, int[] ints) {
+                Editable editable = mEditText.getText();
+                int start = mEditText.getSelectionStart();
+                if (i == Keyboard.KEYCODE_DELETE) {// 回退
+                    if (editable != null && editable.length() > 0) {
+                        if (start > 0) {
+                            editable.delete(start - 1, start);
+                        }
+                    }
+                }else if(i == Keyboard.KEYCODE_CANCEL){
+                    hideKeyboard();
+                }else if (i == 4896) {// 清空
+                    editable.clear();
+                } else { //将要输入的数字现在编辑框中
+                    editable.insert(start, Character.toString((char) i));
+                }
+            }
+
+            @Override
+            public void onText(CharSequence charSequence) {
+
+            }
+
+            @Override
+            public void swipeLeft() {
+
+            }
+
+            @Override
+            public void swipeRight() {
+
+            }
+
+            @Override
+            public void swipeDown() {
+
+            }
+
+            @Override
+            public void swipeUp() {
+
+            }
+        });
+    }
+    private void showKeyboard() {
+        int visibility = keyboardView.getVisibility();
+        if (visibility == View.GONE || visibility == View.INVISIBLE) {
+            keyboardView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void hideKeyboard() {
+        int visibility = keyboardView.getVisibility();
+        if (visibility == View.VISIBLE) {
+            keyboardView.setVisibility(View.INVISIBLE);
+        }
+    }
+}
